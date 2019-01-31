092298bb723730a493b5a268b1fef9a5dfe3d6dc
root <root@root>
2019-01-31 15:37:46 +0100

    back to basics

--- main.rs
+++ main.rs
@@ -3,8 +3,39 @@ extern crate jack;
 
 use std::io;
 
-//mod controller;
-mod handlers;
+struct NotificationHandler {}
+
+impl jack::NotificationHandler for NotificationHandler {
+    fn ports_connected(&mut self, client: &jack::Client, port_id_a: jack::PortId, port_id_b: jack::PortId, are_connected: bool) {
+        // Get ports from client
+        let port_a = client.port_by_id(port_id_a).unwrap();
+        let port_b = client.port_by_id(port_id_b).unwrap();
+
+        // Only interested in our own ports
+        if (client.is_mine(&port_a) || client.is_mine(&port_b)) && are_connected {
+            println!("One of client ports got connected, sending identify request");
+
+            // TODO - send device enquiry
+            //let message = [0xF0, 0x7E, 0x00, 0x06, 0x01, 0xF7];
+        }
+    }
+}
+
+struct ProcessHandler {
+    midi_in: jack::Port<jack::MidiIn>,
+}
+impl jack::ProcessHandler for ProcessHandler {
+    fn process(&mut self, _client: &jack::Client, process_scope: &jack::ProcessScope) -> jack::Control {
+        for event in self.midi_in.iter(process_scope) {
+            println!("{:?}", event);
+        }
+        // Handle input & output in controller
+        //self.controller.process_midi_input(self.midi_in.iter(process_scope));
+        //self.controller.write_midi_output(self.midi_out.writer(process_scope));
+
+        jack::Control::Continue
+    }
+}
 
 fn main() {
     // Setup client
@@ -23,8 +54,10 @@ fn main() {
     //let controller = controller::Controller::new(writer);
 
     // Setup handlers
-    let processhandler = handlers::ProcessHandler::new(&midi_in, &mut midi_out);
-    let notificationhandler = handlers::NotificationHandler{};
+    let processhandler = ProcessHandler{
+        midi_in: midi_in,
+    };
+    let notificationhandler = NotificationHandler{};
 
     // Activate client
     let active_client = client
